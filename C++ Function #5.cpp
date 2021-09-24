6c5fcf6afcda5b6bb0450259aa568bda0d55f84c
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:22:01 +0800

    C++ FUNCTION

---
+++ C++ Function #5.cpp
@@ -0,0 +1,12 @@
+// default values in functions
+#include <iostream>
+using namespace std;
+
+// masukan kode anda disini
+
+int main ()
+{
+  cout << bagi (12) << '\n';
+  cout << bagi (20,4) << '\n';
+  return 0;
+}
