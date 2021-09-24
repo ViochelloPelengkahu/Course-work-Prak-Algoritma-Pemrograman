4ea2c2274915eebc1d681c8340c21e97cf78a597
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:04:13 +0800

    C++ Looping

---
+++ 040 Looping #9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+
+cout << "Data ke-1= ";
+cin >> a;
+cout << "Data ke-2= ";
+cin >> b;
+cout << "Data ke-3= ";
+cin >> c;
+cout << "Jumlah= " << a+b+c << endl;
+cout << "Rata-rata= " << (a+b+c)/3;
+	return 0;
+}
