f4c234156caeac941dcb8dacb979f1b745014d76
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:26:13 +0800

    C++ OUTPUT

---
+++ 9. Variable #1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string prodi ="Prodi IT";
+    int angk = 2021;
+
+    cout<<"Saya Mahasiswa/i "<<prodi <<" UNSRAT angkatan "<<angk<<endl;
+    return 0;
+   
+}
