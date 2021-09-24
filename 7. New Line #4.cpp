be1c05d16011b0e1fb8c4b719eb326578cc371e7
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:23:17 +0800

    C++ OUTPUT

---
+++ 7. New Line #4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World !" << endl;
+    cout << "Saya belajar C++ !" << endl;
+    cout << "The quick brown fox jumps over the lazy dog";
+    return 0;
+}
