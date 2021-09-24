57b04d87dbf310814eb5ccfe6a80c269c5a0ffea
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:09:30 +0800

    C++ NESTED LOOP

---
+++ 044 Star Pattern #3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int x;
+    
+    cout << "Jumlah Baris Bintang = "; cin >> x;
+    
+    
+	for(int i=1;i<=x;i++){
+	    
+		for(int k=1;k<=i;k++){
+		    
+		cout << "*";
+	}
+	
+	
+	cout << endl;
+
+}
+
+
+
+
+
+	cout << "\n\n\n\n\n\n Gak ada yang sama amin.";
+
+return 0;
+}
