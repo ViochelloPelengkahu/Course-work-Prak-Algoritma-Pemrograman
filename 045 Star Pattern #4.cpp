a707605e132c1464b35cc7e8fdde9f0da42ba07f
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:10:46 +0800

    C++ NESTED LOOP

---
+++ 045 Star Pattern #4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a=5;
+	for(int i=1;i<=a;i++){
+		for(int k=1;k<i;k++){
+		cout << " ";
+	}
+	for(int m=a;m>=i;m--){
+		cout << "*";
+	}
+	cout << endl;
+}
+
+for(int k=2;k<=a;k++){
+		for(int m=a;m>k;m--){
+		cout << " ";
+	}
+	for(int r=1;r<=k;r++){
+		cout << "*";
+	}
+	cout << endl;
+}
+
+
+
+
+cout << "\n\n\n\n\n\nGk boleh ada yang sama";
+	return 0;
+}
