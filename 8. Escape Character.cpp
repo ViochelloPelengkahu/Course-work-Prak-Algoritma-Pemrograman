e7c163270bb708d0d7e6bbbc964725493cdcd2e6
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:24:07 +0800

    C++ OUTPUT

---
+++ 8. Escape Character.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Belajar\t" << "C++\n";
+    cout << "Belajar\t \'C++"<< endl;
+    cout << "Belajar\t \"C++\" "<< endl;
+    cout << "\'Belajar\t" << "\"C++\"" <<endl;
+    return 0;
+}
