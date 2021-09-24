202f5f10f6fbd689970ebce3237f05c0f531b62f
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:17:57 +0800

    FUNCTION

---
+++ 052 FUNCTION #1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void cetakpesan(string a){
+    cout << " Ini adalah Fungsi tanpa nilai baik" << endl;
+}
+
+int main()
+{
+    string a;
+  cetakpesan(a);
+  return 0;
+}
