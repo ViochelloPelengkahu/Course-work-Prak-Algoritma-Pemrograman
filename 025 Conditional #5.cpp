18c54a8382ec3f53556b182eb117aea0dd9a2ecc
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:47:16 +0800

    C++ CONDITIONAL

---
+++ 025 Conditional #5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b;
+cout << "Masukkan angka pertama :";
+cin >> a;
+cout << "Masukkan angka kedua : ";
+cin >> b;
+
+if(a<b){ cout <<"Angka pertama lebih kecil dari angka kedua";
+}else{
+	cout << "Angka pertama lebih besar dari angka kedua";
+}
+return 0;
+}
