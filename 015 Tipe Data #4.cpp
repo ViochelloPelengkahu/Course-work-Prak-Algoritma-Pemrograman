6ebd4c2d68a4a38a136d5290bfd2ccf99c67af8b
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:39:02 +0800

    C++ DATA TYPE

---
+++ 015 Tipe Data #4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Kata-Kata \"Keywords\" dalam C++ yang tidak bisa digunakan (variable, fungsi, dll) : ";
+    cout<<"asm else new this auto enum operator throw break export protected try case extern public typedef catch false register typeid char float reinterpret_cast typename class for return union const friend short unsigned continue if sizeof virtual default inline static void delete int static_cast volatile do long struct wchar_t double mutable switch while dynamic_cast namespace template";
+}
