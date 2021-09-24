673af90e722e0c3f07880b3428214ffcbb1e3440
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 19:18:28 +0800

    C++ FUNCTION

---
+++ 053 Function #2.cpp
@@ -0,0 +1,15 @@
+// function example
+#include <iostream>
+using namespace std;
+
+int tambah(int x,int y){
+    int z =x+y;
+    return z;
+}
+
+int main ()
+{
+  int z;
+  z = tambah (5,3);
+  cout << "Hasil penambahan adalah =  " << z;
+}
