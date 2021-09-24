053e66968a3cc8573ed7ff55a3147d0db803673c
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:41:53 +0800

    C++ INPUT

---
+++ 018 Input #2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p,l;
+    cout << "Masukkan Panjang = ";
+    cin >> p;
+    cout << "Masukkan Lebar = ";
+    cin >> l;
+    cout << "Luas Persegi Panjang = " << p*l;
+    return 0;
+}
