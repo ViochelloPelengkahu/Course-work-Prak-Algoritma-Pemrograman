e45a82b378f07e1be3d9f5c46e79487a097cc93d
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:42:40 +0800

    C++ INPUT

---
+++ 019 Input #3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char a;
+    cout << "Masukkan huruf : ";
+    cin >> a;
+    cout << "Nilai ASCII huruf " << a << " = " << a+1-1;
+    return 0;
+}
