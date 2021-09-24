aa27083541019579c27e0f4bd9d04126050cc933
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:33:50 +0800

    C++ VARIABLES

---
+++ 011 Variable #3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+int integeer = 65433;
+float floaat = 123.464;
+char chaar = 'n';
+short shortt = 65;
+cout << "Tipe data integer = " << integeer << endl;
+cout << "	Tipe data float   = " << floaat << endl;
+cout << "Tipe data char    =\n";
+cout << chaar << endl;
+cout << "Tipe data short   = " << shortt << endl;
+return 0;
+}
