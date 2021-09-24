9f602463e3e045641ce99e984601a67bc59e7c1d
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:27:32 +0800

    C++ VARIABLES

---
+++ 10. Variable #2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+    int angkaInt = 5;
+    double angkaDoub = 58.65;
+    char huruf = 'A';
+    string nilai = "Prodi IT UNSRAT";
+    bool bole = false;
+    cout << "\n";
+    cout << "Tipe Data Integer dengan variable angkaInt dan bernilai " << angkaInt << "\n\n";
+    cout << "Tipe Data Double dengan variable angkaDoub dan bernilai " << angkaDoub << "\n\n";
+    cout << "Tipe Data Char dengan variable huruf dan karakter " << huruf << "\n\n";
+    cout << "Tipe Data String dengan variable teks dan teks " << nilai << "\n\n";
+    cout << "Tipe Data Boolean dengan variable bole dan nilai " << bole << "\n\n";
+}
