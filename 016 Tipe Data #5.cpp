76fb1d14531e092dcf760619b8a83365840b97c5
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:39:53 +0800

    C++ DATA TYPE

---
+++ 016 Tipe Data #5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char karakter = 'Z';
+    
+    cout << "Karakter Z = " << karakter << endl;
+    cout << "Nilai ASCII = " << (int) karakter << endl;
+    
+    return 0;
+}
