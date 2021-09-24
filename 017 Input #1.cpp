31cf71a72c6c3b57043140b4803ebe1e1ba52a1c
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:40:39 +0800

    C++ INPUT

---
+++ 017 Input #1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout << "Masukkan Nilai Integer : ";
+    cin >> a;
+    cout <<"Nilai yang anda masukan adalah " << a << " dan nilai dikali 3 adalah " << a*3;
+}
