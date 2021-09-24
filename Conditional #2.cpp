b016b456af97d698976824cef5ad9f6f179b523a
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:45:13 +0800

    C++ CONDITIONAL

---
+++ Conditional #2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a;
+	cout << "Masukan angka : ";
+	cin >> a;
+
+	if(a>=0) {
+		cout << "Anda Memasukan angka Positif !";
+	}else {
+		cout << "Anda Memasukan angka Negatif !";
+	}
+	return 0;
+}
