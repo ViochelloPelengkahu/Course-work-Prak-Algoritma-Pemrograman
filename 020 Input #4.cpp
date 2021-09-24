0e2356b54bba1b64f3dae5b77141113c4bd26067
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2021-09-24 18:43:24 +0800

    C++ INPUT

---
+++ 020 Input #4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=100,b;
+    cout << "Masukkan harga barang: ";
+    cin >> b;
+    cout << "Harga barang setelah pajak: Rp" << a+b;
+}
