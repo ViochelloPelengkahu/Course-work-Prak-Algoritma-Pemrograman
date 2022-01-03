c37ee7138fbbfa78b534028ef53ff272ec8536f5
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2022-01-03 19:09:38 +0800

    C++ ARRAY

---
+++ 094 ARRAY #2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int vio[7] = {54,75,245,764,21,35,43};
+    for(int i=0;i<7;i++){
+        cout << "Nilai ke-" << i+1 <<": " << vio[i] << endl;
+    }
+    
+    return 0;
+}
