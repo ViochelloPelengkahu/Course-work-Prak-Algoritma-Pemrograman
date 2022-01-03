dcc49036f177b1efcb7ccc8c1c7c824227e22183
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2022-01-03 18:55:43 +0800

    C++ OOP

---
+++ 075 DASAR OOP CLASSES #3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Box {
+   public:
+      // Length dari box tipe double
+      // Breadth dari box tipe double
+      // Height dari box tipe double
+};
+
+int main() {
+   // Buat objek box 1 dari kelas Box
+   // Buat objek box 2 dari kelas Box
+   double volume = 0.0;     // variable volume di set ke 0 (awal)
+ 
+   // box 1 specification
+   // buat height box 1 = 5
+   // buat lenght box 1 = 6
+   // buat breadth box 1 = 7
+
+   // box 2 specification
+   //buat height box 2 = 10
+   //buat height box 2 = 12
+   //buat height box 2 = 13
+   
+   
+    // volume (box1) = Box1 height * Box1 length * Box1 breadth
+   cout << "Volume of Box1 : " << volume <<endl; //tampilkan volume box1
+
+   // volume (box2) = Box2 height * Box2 length * Box2 breadth
+   //tampilkan volume box2
+   return 0;
+}
