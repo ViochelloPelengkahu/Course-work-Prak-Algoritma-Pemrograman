80c99a16f93606298c7c9a57fbf9beea7bc7240d
Viochello Chandra Pelengkahu <91269108+ViochelloPelengkahu@users.noreply.github.com>
2022-01-03 19:08:57 +0800

    C++ ARRAY

---
+++ 093 ARRAY #1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // buat array nama huruf panjang 5 bertipe char.. berikan nilai "A" "B"... sampai panjang 5
+    char vio[5] = {'A', 'B', 'C', 'D', 'E'};
+    // mencetak isi array index ke 2 dengan kata "Output = " di depannya 
+    cout << "Output = " << vio[2];
+
+    return 0;
+}
